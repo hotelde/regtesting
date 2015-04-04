4247b4a96ee28d976bddd8c343ed1912d263ed07
Florian Wittmann <Florian.Wittmann@hotel.de>
2015-04-04 02:49:18 +0200

    Add basic testprojecttemplate. Includes just the stuff to load the testproject dynamically.

---
+++ RegTesting.Tests/TypesLoader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using RegTesting.Contracts;
+
+namespace RegTesting
+{
+	/// <summary>
+	/// Loading Types of this project.
+	/// </summary>
+	public class TypesLoader : MarshalByRefObject, ITypesLoader
+	{
+		#region Implementation of ITypesLoader
+
+		/// <summary>
+		/// Get the Types of the Tests Assembly
+		/// </summary>
+		/// <returns>A string array with all types of the assembly</returns>
+		public string[] GetTypes()
+		{
+			try
+			{
+
+				//Find out this assembly.
+				Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().Single(objAssemb => objAssemb.GetName().Name == "RegTesting.Tests");
+				//Get and return all types of this assembly
+				IEnumerable<string> types = assembly.GetTypes().Where(t=>typeof(ITestable).IsAssignableFrom(t) && !t.IsAbstract).Select(objType => objType.FullName);
+				return types.ToArray();
+			}
+			catch (InvalidOperationException invalidOperationException)
+			{
+				//Would be a strange error, but instead of crashing keep it running and report no testcases:
+				Console.Error.WriteLine(invalidOperationException);
+				return new string[0];
+			}
+			catch (ReflectionTypeLoadException reflectionTypeLoadException)
+			{
+				StringBuilder stringBuilder = new StringBuilder();
+				foreach (Exception loaderException in reflectionTypeLoadException.LoaderExceptions)
+				{
+					stringBuilder.AppendLine(loaderException.Message);
+					if (loaderException is FileNotFoundException)
+					{
+						FileNotFoundException exFileNotFound = loaderException as FileNotFoundException;
+						if (!string.IsNullOrEmpty(exFileNotFound.FusionLog))
+						{
+							stringBuilder.AppendLine("Fusion Log:");
+							stringBuilder.AppendLine(exFileNotFound.FusionLog);
+						}
+					}
+					stringBuilder.AppendLine();
+				}
+
+
+
+				string strErrorMessage = stringBuilder.ToString();
+				//Display or log the error based on your application.
+				return new [] {"ERROR:" +  strErrorMessage };
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ RegTesting.Tests/TypesLoaderFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using RegTesting.Contracts;
+
+namespace RegTesting
+{
+	/// <summary>
+	/// A Factory for the Typesloader
+	/// </summary>
+	public class TypesLoaderFactory : MarshalByRefObject , ITypesLoaderFactory
+	{
+		/// <summary>
+		/// Create a Factory
+		/// </summary>
+		/// <param name="strAssemblyFile">Our Assembly</param>
+		/// <param name="strTypeName">the type to create</param>
+		/// <param name="objConstructArgs">constructor arguments</param>
+		/// <returns>A new class of type typeName</returns>
+		public ITypesLoader Create(string strAssemblyFile, string strTypeName,
+								object[] objConstructArgs)
+		{
+			//Ignore all errors and return null in error case.
+			try
+			{
+				return (ITypesLoader)Activator.CreateInstanceFrom(strAssemblyFile, strTypeName, objConstructArgs).Unwrap();
+			}
+			catch (InvalidCastException)
+			{
+				//Try to load and cast of a non ITypesLoader.
+			}
+			catch (MissingMethodException)
+			{
+				//No matching public constructor was found.
+			}
+			catch (TypeLoadException)
+			{
+				//typename was not found in assemblyFile.
+			}
+			catch (System.IO.FileNotFoundException)
+			{
+				//assemblyFile was not found.
+			}
+			catch (MethodAccessException)
+			{
+				// The caller does not have permission to call this constructor.
+			}
+			catch (MemberAccessException)
+			{
+				//Cannot create an instance of an abstract class, or this member was invoked with a late-binding mechanism.
+			}
+			catch (System.Reflection.TargetInvocationException)
+			{
+				//The constructor, which was invoked through reflection, threw an exception.
+			}
+			catch (NotSupportedException)
+			{
+				//activationAttributes is not an empty array, and the type being created does not derive from System.MarshalByRefObject.
+			}
+			catch (System.Security.SecurityException)
+			{
+				//The caller does have the required System.Security.Permissions.FileIOPermission.
+			}
+			catch (BadImageFormatException)
+			{
+				//assemblyFile is not a valid assembly. -or- 
+				//The common language runtime (CLR) version 2.0 or later is currently loaded,
+				//and assemblyName was compiled for a version of the CLR that is later than the currently loaded version.
+				//Note that the .NET Framework versions 2.0, 3.0, and 3.5 all use CLR version 2.0.
+			}
+			return null;
+
+
+
+		}
+	}
+}
