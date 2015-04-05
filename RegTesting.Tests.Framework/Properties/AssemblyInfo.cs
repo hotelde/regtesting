fa664343e8f4b02fdfd6f431a57ee8c1a953168c
Florian Wittmann <dev@florianwittmann.de>
2015-04-06 00:47:09 +0200

    nuspec files for nuget package generation - Created nuspec files for the Tests.Core and the Tests.Framework projects.

--- RegTesting.Tests.Framework/Properties/AssemblyInfo.cs
+++ RegTesting.Tests.Framework/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RegTesting.Tests.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A test framework for tests with selenium.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HOTELDE AG")]
 [assembly: AssemblyProduct("")]
@@ -24,13 +24,10 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
+//      Major Version <- upper this for breaking api changes
+//      Minor Version  <- upper this for new functionality
+//      Build Number <- this should match to the referenced selenium version
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.45.0")]
+[assembly: AssemblyFileVersion("1.0.45.0")]
