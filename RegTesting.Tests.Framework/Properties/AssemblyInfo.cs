78cb708a02f79fc0c0746e8b259ff91780a79889
Florian Wittmann <florian.wittmann@hotel.de>
2016-03-21 15:48:23 +0100

    Bump framework version to 1.3.4

--- RegTesting.Tests.Framework/Properties/AssemblyInfo.cs
+++ RegTesting.Tests.Framework/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("RegTesting.Tests.Framework")]
 [assembly: AssemblyDescription("A test framework for tests with selenium.")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +17,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("a7e6a771-5f9d-425a-a401-3a5f4c71b270")]
 
 // Version information for an assembly consists of the following four values:
@@ -28,9 +30,11 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyFileVersion("1.3.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.3.0")]
+
+[assembly: AssemblyFileVersion("1.3.4.0")]
+[assembly: AssemblyInformationalVersion("1.3.4.0")]
 
 // Change AssemblyVersion for breaking api changes
 // http://stackoverflow.com/questions/64602/what-are-differences-between-assemblyversion-assemblyfileversion-and-assemblyin
+
 [assembly: AssemblyVersion("1.3.0.0")]
